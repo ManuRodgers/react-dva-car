98b3dfab3443b78478c37e97cb757357149e096b
manu <manurodgersgoat@gmail.com>
2018-06-08 23:18:49 +1000

    car show

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import CarPick from "./CarPick";
+import CarShow from "./CarShow";
+import { Table, Divider, Icon, Button, Input } from "antd";
 import "../../styles/less.less";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <CarPick />
+        <CarShow />
       </div>
     );
   }
